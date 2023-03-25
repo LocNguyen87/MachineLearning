f6ec5d10c193523a1a939297576b09b3d14a2982
Loc Nguyen <binbinvn@gmail.com>
2023-03-25 18:08:58 +0700

    update exercise 25/03

---
+++ job.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+data = pd.read_excel('./datasets/final_project.ods', engine='odf', dtype=str)
+target = 'career_level'
+x = data.drop(target, axis=1)
+y = data[target]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100, stratify=y)
+
+vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,1))
+result = vectorizer.fit_transform(data["title"])
+print(result.shape)
